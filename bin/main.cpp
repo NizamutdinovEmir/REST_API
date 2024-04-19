dd5ff8f402fc35df975e266d6dd2748d3df90203
NizamutdinovEmir <144460470+NizamutdinovEmir@users.noreply.github.com>
2024-04-19 20:16:48 +0300

    Add files via upload

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
+#include "../lib/pugixml.hpp"
+#include "../lib/parsing.h"
+#include "../lib/valute.h"
 
+
+
+
+int main(int argc , char** argv) {
+    setlocale(0, "");
+    data_base Data_info;
+    // URL для запроса данных с сайта
+    setup(argc , argv, Data_info);
+
+    return 0;
+}
